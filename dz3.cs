53fe83069af06f27576d987d84c7dee775c71a6d
Xe0n1S <master.5@mail.ru>
2022-10-08 20:13:50 +0400

    dz3

---
+++ dz3.cs
@@ -0,0 +1,64 @@
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. (можно решить через строку, а можно математически)
+
+// Console.WriteLine("Введите 5-ти значное число: ");
+// String n = (Console.ReadLine()!);
+// if ((n.Length == 5))
+// {
+//     if (n[0]==n[4] && n[1]==n[3])
+//     {
+//         Console.WriteLine("Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine("НЕ палиндром");
+//     }
+// }
+
+// else
+// {
+//     Console.WriteLine($"число не 5-ти значное!");
+// }
+
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.WriteLine("Введите координаты XA: ");
+// int xa = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координаты YA: ");
+// int ya = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координаты ZA: ");
+// int za = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координаты XB: ");
+// int xb = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координаты YB: ");
+// int yb = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите координаты ZB: ");
+// int zb = int.Parse(Console.ReadLine()!);
+
+// double result = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
+// Console.WriteLine(result);
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.WriteLine("Введите число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int x = 1;
+// if (N < 1)
+// {
+//     Console.WriteLine("Неверное число");
+// }
+// else
+// {
+//     while (x <= N)
+//     {
+//         Console.WriteLine($"{Math.Pow(x, 3)}");
+//         x++;
+//     }
+// }
+
+
